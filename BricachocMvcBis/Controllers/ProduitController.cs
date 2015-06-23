4fabbca05f92399e8042c514035c12ee7e0cdde3
yetelday <yetelday@gmail.com>
2015-06-23 16:24:39 +0200

    1er_envoi

---
+++ BricachocMvcBis/Controllers/ProduitController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BricachocBll;
+using BricachocBo;
+
+namespace BricaChocMVC.Controllers
+{
+    public class ProduitController : Controller
+    {
+        // GET: Produit
+        public ActionResult Index()
+        {
+            GestionnaireGererProduit GesProd = new GestionnaireGererProduit();
+            ICollection<Produit> CollecProd = new Collection<Produit>();
+            CollecProd = GesProd.ChargerLesProduits();
+            return View(CollecProd);
+        }
+
+        // GET: Produit/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produit/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produit/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produit/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produit/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produit/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produit/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
